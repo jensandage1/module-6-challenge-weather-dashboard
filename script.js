348dcd1a803176604d53cff0445967136c6eaa5d
Jen Sandage <jensandage1@gmail.com>
2023-06-10 22:28:43 -0700

    pulls data, logs in the console

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var APIKey = "4023e75f24d6e229beec978ad1d80ad9"; 
-var cityName = document.getElementById("search-city"); //<-not working to pull from input 
-var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`; 
 
+var searchBtn = document.getElementById("search-button");
+var inputField = document.getElementById("search-city");
+var cityName;
+
+searchBtn.onclick = getInput;
+
+function getInput(){
+    cityName = inputField.value;
+    getApi();
+    return cityName;
+}
 
 function getApi() {
+    var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`; 
     fetch(queryUrl)
     .then(function (response) {
         return response.json();
@@ -12,18 +22,38 @@ function getApi() {
         console.log(data); // This will output the data you got from the API to the console.
         var currentInfo = document.getElementById("city-conditions");//i want the current conditions for searched city to go here.
         currentInfo.innterHTML= data; 
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        getWeather(lat,lon);
     })
     .catch(function (error) {
         console.log('Error:', error); // If something goes wrong, this will output the error.
-        var logErr = document.getElementById("cityconditions");
+        var logErr = document.getElementById("city-conditions");
+        //var logErr = document.getElementById("search-city");
         logErr.textContent = "please check your spelling and try again."
     });
-   
 }
-getApi();
+
+function getWeather(lat, lon) {
+    var queryUrl2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+    fetch(queryUrl2)
+    .then(function (response) {
+        return response.json();
+    })
+    .then (function (data2) {
+        console.log(data2);
+    })
+   // for(var i=0; i < data2.length; i+8) {
+    //data2.value[i];
+   // }
+
+}
+
+//`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+//getApi();
 
 function searchCity(){
-    $("#container > .search-button").click(() => {
+    $(".flex-container > .search-button").click(() => {
     let textInput = $("#container > #search-city").val();
     localStorage.setItem("city" , textInput)
 })
@@ -32,7 +62,7 @@ searchCity();
 
 
 function displayPreviousCity(){
-    $("#container > #search-city").val(localStorage.getItem("city"));
+    $(".flex-container > #search-city").val(localStorage.getItem("city"));
 }
 displayPreviousCity();
 
