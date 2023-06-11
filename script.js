ffdf9eecf4826e3950b3a0251d02ba33d1b95f08
Jen Sandage <jensandage1@gmail.com>
2023-06-11 16:36:04 -0700

    most recent item stored in local storage and displayed on page upon reloading

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var inputField = document.getElementById("search-city");
 var cityName;
 
 
+
 searchBtn.onclick = getInput;
 
 function getInput(){
@@ -61,40 +62,72 @@ function getWeather(lat, lon) {
      var dailyForecast = dateAndTime2 + "Temperature (F): " + temp2 + "Wind: " + wind2 + "humidity: " + humidity2;  
         
         console.log(data2);
+    var fiveDayForecast1 = document.getElementById("five-day-forecast1");  
+        fiveDayForecast1.innerText= dailyForecast;
 
-
-    //for(let i=0; i < data2.list.length; i+=8) {
+    //for(let i=8; i < data2.list.length; i+=8) {
     //var forecastAll = document.querySelector(".forecast");
     //forecastAll.innerText = data2.list[i];
-    var fiveDayForecast1 = document.getElementById("five-day-forecast1");  
-    fiveDayForecast1.innerText= dailyForecast;
 
-
-    var temp22 = data2.list[8].main.temp;
-        var wind22 = data2.list[8].wind.speed;
-        var humidity22 = data2.list[8].main.humidity;
-        var dateAndTime22 = data2.list[8].dt_txt;
-        JSON.parse(temp22, wind22, humidity22, dateAndTime22);
-     var dailyForecast2 = dateAndTime22 + "Temperature (F): " + temp22 + "Wind: " + wind22 + "humidity: " + humidity22;  
+    var temp3 = data2.list[8].main.temp;
+        var wind3 = data2.list[8].wind.speed;
+        var humidity3 = data2.list[8].main.humidity;
+        var dateAndTime3 = data2.list[8].dt_txt;
+        JSON.parse(temp3, wind3, humidity3, dateAndTime3);
+     var dailyForecast2 = dateAndTime3 + "Temperature (F): " + temp3 + "Wind: " + wind3 + "humidity: " + humidity3;  
     var fiveDayForecast2 = document.getElementById("five-day-forecast2");
     fiveDayForecast2.innerText = dailyForecast2;
-    });
+   
+    var temp4 = data2.list[16].main.temp;
+        var wind4 = data2.list[16].wind.speed;
+        var humidity4 = data2.list[16].main.humidity;
+        var dateAndTime4 = data2.list[16].dt_txt;
+        JSON.parse(temp4, wind4, humidity4, dateAndTime4);
+     var dailyForecast3 = dateAndTime4 + "Temperature (F): " + temp4 + "Wind: " + wind4 + "humidity: " + humidity4;  
+    var fiveDayForecast3 = document.getElementById("five-day-forecast3");
+    fiveDayForecast3.innerText = dailyForecast3;
+
+    var temp5 = data2.list[24].main.temp;
+        var wind5 = data2.list[24].wind.speed;
+        var humidity5 = data2.list[24].main.humidity;
+        var dateAndTime5 = data2.list[24].dt_txt;
+        JSON.parse(temp5, wind5, humidity5, dateAndTime5);
+     var dailyForecast4 = dateAndTime5 + "Temperature (F): " + temp5 + "Wind: " + wind5 + "humidity: " + humidity5;  
+    var fiveDayForecast4 = document.getElementById("five-day-forecast4");
+    fiveDayForecast4.innerText = dailyForecast4;
+
+    var temp6 = data2.list[32].main.temp;
+        var wind6 = data2.list[32].wind.speed;
+        var humidity6 = data2.list[32].main.humidity;
+        var dateAndTime6 = data2.list[32].dt_txt;
+        JSON.parse(temp6, wind6, humidity6, dateAndTime6);
+     var dailyForecast5 = dateAndTime6 + "Temperature (F): " + temp6 + "Wind: " + wind6 + "humidity: " + humidity6;  
+    var fiveDayForecast5 = document.getElementById("five-day-forecast5");
+    fiveDayForecast5.innerText = dailyForecast5;
+    //I know writing them all out is not the easiest way to do this, but I couldn't get the for loop to work. 
+});
 }
 
-function searchCity(){
-    $(".flex-container > .search-button").click(() => {
-    let textInput = $("#container > #search-city").val();
-    localStorage.setItem("city" , textInput)
-})
-}
-searchCity();
-
+var previousCity = document.getElementById("list-item1")
 
 function displayPreviousCity(){
-    $(".flex-container > #search-city").val(localStorage.getItem("city"));
+    let displayOutput = localStorage.getItem("nameOfCity");
+    previousCity.innerText = displayOutput;
 }
 displayPreviousCity();
 
-//click event - display searched city current and 5 day forecast
-//save searched city to local storage
+searchBtn.addEventListener("click", function setCity () {
+    let textInput = inputField.value;
+    localStorage.setItem("nameOfCity", textInput);
+});
+
+
+
+
+
+
+
+
+//click event - display searched city current and 5 day forecast -yes
+//save searched city to local storage - yes
 //get from local storage and display last 5 searched cities.
