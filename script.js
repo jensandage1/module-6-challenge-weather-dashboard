a766da7dddc6627ab47c33d0e80b8dbfdf790f61
Jen Sandage <jensandage1@gmail.com>
2023-06-13 17:45:55 -0700

    everything is working, need to uppercase first letter of displayed cities and add icons, then just make it look nicer

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ function getApi() {
         var currentHumidity = document.getElementById("humidity");
         currentHumidity.innerText = "Humidity: " + humidity1;
         var cityNameDisplayed = document.getElementById("city-and-date");
-        cityNameDisplayed.innerText = name1;
+        var currentDate = dayjs().format('MM/DD/YYYY');
+        cityNameDisplayed.innerText = name1 + " - " +  currentDate ;
         var lat = data.coord.lat;
         var lon = data.coord.lon;
         getWeather(lat,lon);
@@ -59,8 +60,9 @@ function getWeather(lat, lon) {
         var wind2 = data2.list[0].wind.speed;
         var humidity2 = data2.list[0].main.humidity;
         var dateAndTime2 = data2.list[0].dt_txt;
+        var dateOnly = dateAndTime2.split(" ")[0] ;
         JSON.parse(temp2, wind2, humidity2, dateAndTime2);
-     var dailyForecast = dateAndTime2 + "\n" + "Temperature (F): " + temp2 + "\n" +  " Wind: " + wind2 + "\n" + " humidity: " + humidity2;  
+     var dailyForecast = dateOnly + "\n" + "Temp (F): " + temp2 + "\n" +  " Wind: " + wind2 + " MPH" + "\n" + " Humidity: " + humidity2 + "%";  
         
         console.log(data2);
     var fiveDayForecast1 = document.getElementById("five-day-forecast1");  
@@ -74,17 +76,20 @@ function getWeather(lat, lon) {
         var wind3 = data2.list[8].wind.speed;
         var humidity3 = data2.list[8].main.humidity;
         var dateAndTime3 = data2.list[8].dt_txt;
+        var dateOnly = dateAndTime3.split(" ")[0] ;
         JSON.parse(temp3, wind3, humidity3, dateAndTime3);
-     var dailyForecast2 = dateAndTime3 + "\n" + "Temperature (F): " + temp3 +  "\n" +"Wind: " + wind3 + "\n" + "humidity: " + humidity3;  
+     var dailyForecast2 = dateOnly + "\n" + "Temp (F): " + temp3 +  "\n" +"Wind: " + wind3 + " MPH" + "\n" + "Humidity: " + humidity3 + "%";  
     var fiveDayForecast2 = document.getElementById("five-day-forecast2");
     fiveDayForecast2.innerText = dailyForecast2;
+
    
     var temp4 = data2.list[16].main.temp;
         var wind4 = data2.list[16].wind.speed;
         var humidity4 = data2.list[16].main.humidity;
         var dateAndTime4 = data2.list[16].dt_txt;
+        var dateOnly = dateAndTime4.split(" ")[0] ;
         JSON.parse(temp4, wind4, humidity4, dateAndTime4);
-     var dailyForecast3 = dateAndTime4 + "\n" + "Temperature (F): " +  temp4 + "\n" + "Wind: " + wind4 + "\n" + "humidity: " + humidity4;  
+     var dailyForecast3 = dateOnly + "\n" + "Temp (F): " +  temp4 + "\n" + "Wind: " + wind4 + " MPH" + "\n" + "Humidity: " + humidity4 + "%";  
     var fiveDayForecast3 = document.getElementById("five-day-forecast3");
     fiveDayForecast3.innerText = dailyForecast3;
 
@@ -92,8 +97,9 @@ function getWeather(lat, lon) {
         var wind5 = data2.list[24].wind.speed;
         var humidity5 = data2.list[24].main.humidity;
         var dateAndTime5 = data2.list[24].dt_txt;
+        var dateOnly = dateAndTime5.split(" ")[0] ;
         JSON.parse(temp5, wind5, humidity5, dateAndTime5);
-     var dailyForecast4 = dateAndTime5 + "\n" + "Temperature (F): " + temp5 + "\n" + "Wind: " + wind5 + "\n" + "humidity: " + humidity5;  
+     var dailyForecast4 = dateOnly + "\n" + "Temp (F): " + temp5 + "\n" + "Wind: "  + wind5 + " MPH" +  "\n" + "Humidity: " + humidity5 + "%";  
     var fiveDayForecast4 = document.getElementById("five-day-forecast4");
     fiveDayForecast4.innerText = dailyForecast4;
 
@@ -101,8 +107,9 @@ function getWeather(lat, lon) {
         var wind6 = data2.list[32].wind.speed;
         var humidity6 = data2.list[32].main.humidity;
         var dateAndTime6 = data2.list[32].dt_txt;
+        var dateOnly = dateAndTime6.split(" ")[0] ;
         JSON.parse(temp6, wind6, humidity6, dateAndTime6);
-     var dailyForecast5 = dateAndTime6 + "\n" + "Temperature (F): " + temp6 + "\n" + "Wind: " + wind6 + "\n" + "humidity: " + humidity6;  
+     var dailyForecast5 = dateOnly + "\n" + "Temp (F): " + temp6 + "\n" + "Wind: " + " MPH" + wind6 + "\n" + "Humidity: " + humidity6 + "%";  
     var fiveDayForecast5 = document.getElementById("five-day-forecast5");
     fiveDayForecast5.innerText = dailyForecast5;
     //I know writing them all out is not the easiest way to do this, but I couldn't get the for loop to work. 
@@ -126,12 +133,35 @@ function setCity() {
 
 
 function displaySearchedCities() {
+     var cityElement = $("#previous-cities-list");
+        cityElement.empty();
     for (let i=0; i < searchedCities.length; i++) {
-        var cityElement = document.getElementById("list-item" + (i + 1));
+        //first loop looks for element with id "list-item1"
+        //console.log(cityElement);
+        var listItem = $("<li></li>");
+        listItem.text(searchedCities[i]);
+        //console.log(searchedCities[i]);
+        cityElement.append(listItem);
+        $(listItem).on("click", function(){
+    cityName = searchedCities[i];
+    getApi();
+        });
 
-        cityElement.innerText = searchedCities[i];
+    
+        
     }
 }
+
+function formatDate(inputDate) {
+    var date = new Date(inputDate);
+    console.log(date);
+    if (!isNaN(date.getTime())) {
+        // Months use 0 index.
+        return date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
+    }
+}
+
+
 
 displaySearchedCities();
 
